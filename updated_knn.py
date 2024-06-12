3876b4721f7cfbf7fd49657abe81165f032330ae
ammarabdalghaffar <94454836+ammarabdalghaffar@users.noreply.github.com>
2024-06-13 02:30:18 +0300

    Update updated_knn.py

--- updated_knn.py
+++ updated_knn.py
@@ -88,5 +88,5 @@ def get_data():
     return jsonify(whole_predection)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 4000))
     app.run(host='0.0.0.0', port=port, debug=False)
